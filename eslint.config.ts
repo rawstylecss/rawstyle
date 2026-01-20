b3d094c99702ba7549b17d9c8aa77fd874107277
kh4f <kh4f.dev@gmail.com>
2026-01-20 10:36:28 +0300

    chore(eslint): add initial ESLint configuration for TypeScript
    - Introduce `eslint.config.ts` with base, type-aware, and stylistic rules.
    - Update `package.json` with ESLint-related dev dependencies and a lint script.

---
+++ eslint.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+	globalIgnores(['dist'], 'Global Ignores'),
+	{
+		name: 'Base Rules',
+		files: ['**/*.ts'],
+		extends: [eslint.configs.recommended],
+	},
+	{
+		name: 'Type-Aware Rules',
+		files: ['**/*.ts'],
+		extends: [tseslint.configs.strictTypeChecked, tseslint.configs.stylisticTypeChecked],
+		languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname } },
+		rules: {
+			'@typescript-eslint/no-confusing-void-expression': ['error', { ignoreArrowShorthand: true }],
+			'@typescript-eslint/restrict-template-expressions': 'off',
+		},
+	},
+	{
+		name: 'Stylistic Rules',
+		files: ['**/*.ts'],
+		extends: [stylistic.configs.recommended],
+		rules: {
+			'@stylistic/indent': ['error', 'tab'],
+			'@stylistic/indent-binary-ops': ['error', 'tab'],
+			'@stylistic/no-tabs': 'off',
+			'@stylistic/eol-last': ['error', 'never'],
+			'@stylistic/brace-style': ['error', '1tbs'],
+			'@stylistic/arrow-parens': ['error', 'as-needed'],
+		},
+	},
+])
