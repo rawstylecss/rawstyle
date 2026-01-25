04023bb6b655d3bbc3174977c318568085813973
kh4f <kh4f.dev@gmail.com>
2026-01-25 13:37:23 +0300

    feat(rawstyle): expose `TRANSFORMABLE_EXTENSIONS` for plugin usage
    Add the `TRANSFORMABLE_EXTENSIONS` constant to the `rawstyle` package, allowing related plugins to reference a single source of truth for supported file extensions.

--- packages/rawstyle/src/index.ts
+++ packages/rawstyle/src/index.ts
@@ -2,6 +2,7 @@ import { parseSync, Visitor } from 'oxc-parser'
 import { generateHash } from '@/utils'
 import type { TransformResult, ClassNameId, CssVar, Replacement } from '@/types'
 
+export const TRANSFORMABLE_EXTENSIONS = ['.tsx']
 export const VIRTUAL_PREFIX = 'virtual:rawstyle/'
 export const RESOLVED_PREFIX = `\0${VIRTUAL_PREFIX}`
 
