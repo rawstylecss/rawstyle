d1a91b7d9b2387677767c7f0153ed53b2b91481d
kh4f <kh4f.dev@gmail.com>
2026-01-26 09:01:27 +0300

    refactor(next)!: migrate to new Rawstyle v0.2 API and require new peer version
    BREAKING CHANGE: The plugin now works with the updated Rawstyle API and requires `rawstyle` >=0.2 as a peer dependency.

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,7 +1,7 @@
 import { transform } from 'rawstyle'
 
 export default function (this: { resourcePath: string }, source: string): string {
-	const { transformedCode, extractedCss } = transform(this.resourcePath, source)
-	const base64Css = Buffer.from(extractedCss, 'utf8').toString('base64')
-	return `import "data:text/css;base64,${base64Css}";${transformedCode}`
+	const { transformed, css } = transform(this.resourcePath, source)
+	const base64Css = Buffer.from(css, 'utf8').toString('base64')
+	return `import "data:text/css;base64,${base64Css}";${transformed}`
 }
