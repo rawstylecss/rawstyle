f825ed1e3c2308b87d5137fbfdb229a7a43f0df1
kh4f <kh4f.dev@gmail.com>
2026-01-26 09:03:42 +0300

    style(next): use single quotes for import statement
    Replaced double quotes with single quotes in the Next.js plugin for consistency.

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -3,5 +3,5 @@ import { transform } from 'rawstyle'
 export default function (this: { resourcePath: string }, source: string): string {
 	const { transformed, css } = transform(this.resourcePath, source)
 	const base64Css = Buffer.from(css, 'utf8').toString('base64')
-	return `import "data:text/css;base64,${base64Css}";${transformed}`
+	return `import 'data:text/css;base64,${base64Css}';${transformed}`
 }
