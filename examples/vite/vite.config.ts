15a4fc6fe1e451d766b218ed85232b3cc3c49f96
kh4f <kh4f.dev@gmail.com>
2026-01-26 12:53:44 +0300

    style(examples/vite): correct typo in import statement for `rawstyle`
    Fixes the import statement by correcting the spelling of `rawstyle` in the Vite configuration file.

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -1,9 +1,9 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'rolldown-vite'
 import react from '@vitejs/plugin-react'
-import rawtyle from '@rawstyle/vite'
+import rawstyle from '@rawstyle/vite'
 
 export default defineConfig({
 	resolve: { alias: {	'@': resolve('src') } },
-	plugins: [react(), rawtyle()],
+	plugins: [react(), rawstyle()],
 })
