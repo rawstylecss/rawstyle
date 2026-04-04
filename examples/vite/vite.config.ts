9f3de12b98191c5bdfbd9a72508971d05b5873f3
kh4f <kh4f.dev@gmail.com>
2026-04-04 06:48:26 +0300

    chore(examples): update dev deps

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
 import rawstyle from '@rawstyle/vite'
-import type { UserConfig } from 'rolldown-vite'
+import type { UserConfig } from 'vite'
 
 export default {
 	resolve: { alias: {	'@': resolve('src') } },
